8fc51e4219f890a6f8405ddc2ca6e8398593d5ab
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2023-07-05 18:43:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-import streamlit as st
 import google.generativeai as palm
+import numpy as np
 import pandas as pd
+from scipy.spatial.distance import cosine
+import streamlit as st
 
+# Backend
 palm_api_key = st.secrets["PALM_API_KEY"]
 def call_palm(prompt: str, palm_api_key: str) -> str:
     palm.configure(api_key=palm_api_key)
@@ -14,8 +17,49 @@ def call_palm(prompt: str, palm_api_key: str) -> str:
 
     return completion.result
 
-df = pd.read_csv("data.csv")
+def calculate_cosine_similarity(sentence1: str, sentence2: str) -> float:
+    """
+    Calculate the cosine similarity between two sentences.
+
+    Args:
+        sentence1 (str): The first sentence.
+        sentence2 (str): The second sentence.
+
+    Returns:
+        float: The cosine similarity between the two sentences, represented as a float value between 0 and 1.
+    """
+    # Tokenize the sentences into words
+    words1 = sentence1.lower().split()
+    words2 = sentence2.lower().split()
+
+    # Create a set of unique words from both sentences
+    unique_words = set(words1 + words2)
+
+    # Create a frequency vector for each sentence
+    freq_vector1 = np.array([words1.count(word) for word in unique_words])
+    freq_vector2 = np.array([words2.count(word) for word in unique_words])
+
+    # Calculate the cosine similarity between the frequency vectors
+    similarity = 1 - cosine(freq_vector1, freq_vector2)
+
+    return float(similarity)
+
+
+# Load data
+df = pd.read_csv("question_answer_data_set_list.csv")
+df['sim_score'] = df.apply(lambda x: calculate_cosine_similarity(x['question'], user_question), axis=1)
+df = df.sort_values(by='sim_score', ascending=False)
+context = df['answers'].iloc[0:3]
 st.dataframe(df)
+
+# UI
 user_question = st.text_input('Enter a question:', 'Tell me a joke.')
-answer = call_palm(prompt=user_question, palm_api_key=palm_api_key)
+
+# Prompt engineer
+engineered_prompt = f"""
+    Based on the context: {context}, 
+    answer the following question: {user_question}
+"""
+
+answer = call_palm(prompt=engineered_prompt, palm_api_key=palm_api_key)
 st.write('Answer:', answer)
